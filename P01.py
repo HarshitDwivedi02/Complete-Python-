0ffa992f69d832921d75c7df466aa11b181cb044
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2024-06-22 13:24:43 +0530

    First python

---
+++ P01.py
@@ -0,0 +1,20 @@
+# Chapter 1
+
+# Practice Set 1
+
+# Q1 print twinkle twinkle 
+
+#print("Twinkle Twinkle Little Stars")
+
+
+# Q2 print 5 table
+
+#print(5*1 , 5*2)
+
+# Q3 import module
+#from playsound import playsound
+
+# Q4 list directory things
+import os
+print(os.listdir()) 
+
