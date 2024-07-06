17e18e2ba0d682ecd39fd722e5ad3785eecb2647
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2024-07-06 20:26:44 +0530

    Operators in Python

---
+++ operators.py
@@ -0,0 +1,74 @@
+# Arithmetic operators 
+'''
+a=20
+b=3
+print(a+b,a-b,a*b,a/b)
+print(a%b,a**b,a//b) 
+
+# Assignment Operator 
+
+a, b, c, d, e = 30,67,45,33,22
+print(a,b,c,d,e)
+
+a += 5
+b -= 53
+c **= 3
+d %= 10
+e //= 3
+print(a, b, c, d, e) 
+
+# Comparison Operator 
+
+a = 45
+b = 23
+print("a == b is", a == b)
+print("a != b is", a != b)
+print("a > b is", a > b)
+print("a < b is", a < b)
+print("a >= b is", a >= b)
+print("a <= b is", a <= b) 
+
+#Logical Operator 
+
+a = 45
+b = 23
+print(a > 33 and b > 33)
+print(a > 33 and b < 33)
+print(a >= 45 and b <= 23)
+print(a < 100 or b > 50)
+print(a < 1 or b < 11)
+print(a < 1 and b < 11)
+print(not (a > 0 and b > 0)) 
+
+# IF-ELSE
+
+var1 = 5
+var2= 10
+
+if var1>var2:
+    print("Var1 is greater.")
+else:
+    print("Var2 is greater.") 
+
+var1 = 5
+var2 = 5
+if var1>var2:
+    print("Var1 is greater.")
+elif var2>var1:
+    print("Var2 is greater.")
+else:
+    print("They both are equal.") 
+
+# AND Operator
+a = 99
+b = 33
+c = 412
+if a > b and c > a:
+    print("Both conditions are True") 
+
+# Or operator
+a = 41
+b = 33
+c = 500
+if a > b or a > c:
+    print("At least one of the conditions is True") '''
