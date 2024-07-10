96922b3af0b9e02f6fdd16409bfd7402481e0cae
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2024-07-10 22:38:32 +0530

    Tuples

---
+++ tuples.py
@@ -0,0 +1,67 @@
+# Tuples 
+'''
+# Syntax to create a tuple
+data = (43,"Ram",34,59)
+print(data)
+print(type(data)) 
+
+# Another method to create without using brackets
+data2 = 65,43,"Ram","Python"
+print(data2)
+print(type(data2)) 
+
+# Empty tuple
+tuple1 = ()
+print(tuple1)
+# Python Single Object Tuple Example
+# For a single valued tuple, there must be a comma at the end of the value.
+tuple1 = (10,)
+print(tuple1) 
+
+# Tuple of tuples
+tupl1 = 'a', 'mahesh', 10.56
+tupl2 = tupl1, (10, 20, 30)
+print(tupl1)
+print(tupl2) 
+
+# Accessing Tuples 
+data1 = (1, 2, 3, 4)
+data2 = ('x', 'y', 'z')
+print(data1[0])
+print(data1[0:2])
+print(data2[-3:-1])
+print(data1[0:])
+print(data2[:2]) 
+
+# Tuple doesn't support modification or updating in the eisting elements
+
+data = (54, "Python", 21, 43, 54, "87", "Language")
+data[3] = 100
+print(data) 
+
+# Operations over tuples 
+
+# 1 Addition operator in tuplez
+
+data1 = (54, "Python", 21, 43, 54, "87", "Language")
+data2 = (65, 11, "Computer", "Cycle", 76, 88)
+data3 = data1 + data2
+print(data1)
+print(data2)
+print(data3) 
+
+# 2 Replicating tuples i.e multiplying it several times 
+
+data1 = (54, "Python", 21, 43, 54, "87", "Language")
+data2 = (65, 11, "Computer", "Cycle", 76, 88)
+data3 = data1 * 2
+data4 = data2 * 3
+print(data1)
+print(data2)
+print(data3)
+print(data4) ''' 
+
+# 4 Python tuple slicing 
+
+
+
