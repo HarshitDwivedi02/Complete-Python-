5f453f99769248b29aab939a75b80f48eea339dd
Harshit Dwivedi <harshitdwivedi2002@gmail.com>
2025-09-21 14:14:14 +0530

    Dictionary Update

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+# # Dictionary in Python 
+
+# mydict = {"Key1":"Value1","Key2":"Value2"}
+# print(mydict) 
+
+# print(type(mydict)) 
+
+# student_info = {"name": "James", "roll": 45, "gender": "Male"}
+# print(student_info)
+# student_info = dict({"name": "James", "roll": 45, "gender":
+# "Male"})
+# print(student_info)
+# student_info = dict([("name", "James"), ("roll", 45),
+# ("gender", "Male")])
+# print(student_info)
+# student_info = dict(name="James", roll=45, gender="Male")
+# print(student_info)
+
+print("Harshit Dwivedi")
